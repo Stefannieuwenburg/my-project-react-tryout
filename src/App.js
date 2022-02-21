d78ef7e53e8b578a9939c83f807deb655bcbde41
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-02-21 12:24:39 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <a>
           Learn React
         </a>
